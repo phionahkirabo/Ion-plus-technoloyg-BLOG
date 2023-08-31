cbbf19a98e536b9ef98e4995372e58b31ad0e2d5
phionah <pkirabo221@daviscollege.com>
2023-08-31 02:55:52 -0700

    show is working

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,53 +1,15 @@
-
 @extends('auth.dashbord')
 @section('content')
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+<div style="text-align: center; margin: 20px; padding: 10px; background-color: #f0f0f0;">
+    <h1>This is Centered Text</h1>
+    <p>This text is centered within the div and has margin and padding.</p>
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
-</head>
-<body>
-
-<h2>Details of single blog</h2>
-<div style="margin-right:10%;float: right;">
-    <a href="{{url('addpost')}}">Add</a>
+     
+        <h1>Title:{{ $show->title }}</h1>
+        <p>Content:{{$show->content}}</p>
+        <p>published date:{{$show->published_at }}</p>
+        <p>postcreated date:{{$show->published_at }}</p>
+       
+        <a href="{{url('allblogs')}}" class="btn btn-dange"><button>Back</button></a>
 </div>
-<table>
-  <tr>
-    <th>#</th>
-    <th>Name</th>
-    <th>Email</th>
-    <th>Phone</th>
-    <th>Address</th>
-    <!-- <th colspan="3">ACTION</th> -->
-  </tr>
-@foreach ($show as $i=> $blog)
-@if ($blog)
-    <tr>
-      <td>{{ ++$i }}</td>
-      <td>{{ $blog->title }}</td>
-      <td>{{ $blog->content }}</td>
-      <td>{{ $blog->published_at }}</td>
-      <td>{{ $blog->postcreated_at }}</td>
-    </tr>
-  @else
-    <tr>
-      <td colspan="5">No blog data available.</td>
-    </tr>
-  @endif
-</table>
-
 @endsection 
